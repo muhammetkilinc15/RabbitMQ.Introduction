edffdc1e4f7447695066056bb0278667653f5d1e
Muhammet Kılınc <108901980+muhammetkilinc15@users.noreply.github.com>
2025-03-15 22:02:43 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RabbitMQ.Client.Events;
+
+// 1- Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://eoetypiy:O5oUx3bmcItd-i9gf9SFEtn4eDZduJyy@cow.rmq2.cloudamqp.com/eoetypiy");
+
+// 2- Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connetion = await factory.CreateConnectionAsync();
+using IChannel channel = await connetion.CreateChannelAsync();
+
+// 3- Kuyruk Dinleme
+await channel.QueueDeclareAsync(queue: "example-queue", true, exclusive: false, false, null);
+
+// 4- Mesaj Alımı
+AsyncEventingBasicConsumer consumer = new(channel);
+await channel.BasicConsumeAsync(queue: "example-queue", false, consumer);
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Gelen Mesaj: " + message);
+    await channel.BasicAckAsync(eventArgs.DeliveryTag, false);
+};
+
+await channel.BasicConsumeAsync("example-queue", true, consumer);
+
+// 5- Bağlantıyı Kapatma
+Console.ReadLine();
+await channel.CloseAsync();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿// 1- Bağlantı Oluşturma
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://eoetypiy:O5oUx3bmcItd-i9gf9SFEtn4eDZduJyy@cow.rmq2.cloudamqp.com/eoetypiy");
+
+// 2- Bağlantı Aktifleştirme ve Kanal Açma
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+// 3- Kuyruk Oluşturma
+await channel.QueueDeclareAsync(queue: "hello", true, exclusive: false, false, null);
+
+string input;
+do
+{
+    // 4- Mesaj Gönderme
+    Console.Write("Enter message (type 'exit' to quit): ");
+    input = Console.ReadLine();
+    if (!string.IsNullOrEmpty(input) && !input.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
+    {
+        byte[] message = Encoding.UTF8.GetBytes(input);
+        await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message);
+    }
+} while (!string.IsNullOrEmpty(input) && input.ToLower() != "exit");
+
+// 5- Bağlantıyı Kapatma
+await channel.CloseAsync();
